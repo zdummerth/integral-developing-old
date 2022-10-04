d0efe18149d2dcf12cf384fa26638f71f8bf8183
unknown <zach>
2022-10-03 17:47:37 -0700

    hook up database and move home page data to database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { GetStaticProps } from "next";
 import transformContent from "../lib/transform-content";
+import { getPageByHandle } from "../lib/callFauna";
 import getNavData from "../lib/get-navigation-data";
 import Page from "../components/Page";
 
@@ -17,79 +18,15 @@ const Home = (props: any) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const sections = [
-    {
-      type: "div",
-      className:
-        "w-full md:flex md:items-center bg-gradient-to-br from-black via-black to-emerald-500 font-sans",
-      children: [
-        {
-          type: "image",
-          imageUuid: "fd3431aa-386f-4f77-ae4e-4b1639f75c87",
-          image: {
-            alttext: "Disc Market Logo",
-            objectFit: "contain",
-            layout: "responsive",
-          },
-          wrapperClassName: "relative w-full",
-        },
-        {
-          type: "div",
-          className: "w-full text-center max-w-2xl p-4",
-          children: [
-            {
-              type: "h1",
-              value: "The Disc Market",
-              className: "text-3xl font-serif",
-            },
-            {
-              type: "h3",
-              value: "Community and Sustainability",
-              className: "text-xl my-2 font-serif",
-            },
-            {
-              type: "p",
-              value:
-                "At The Disc Market, we believe in giving back to the communities that provide so much to the game of disc golf. That is why 75% of all profits are donated to local clubs and new course development.",
-              className: "my-2",
-            },
-            {
-              type: "p",
-              value:
-                "We also strive to make our products in the most sustainable way possible. We do this by creating products on demand and using organic or recycled material for our apparel.",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      type: "div",
-      className: "w-full my-8 p-2",
-      children: [
-        {
-          type: "h2",
-          value: "Featured Products",
-          className: "text-2xl",
-        },
-        {
-          type: "product_list_by_tags",
-          tags: ["Featured"],
-          config: {
-            enlarge_first: "false",
-            action: "standard",
-          },
-        },
-      ],
-    },
-  ];
-
+export const getStaticProps: GetStaticProps = async () => {
   const seo = {
     title: "Home",
     description: "Welcome to the disc market",
   };
 
-  const content = await transformContent(sections);
+  const pageData: any = await getPageByHandle({ handle: "/" });
+
+  const content = await transformContent(pageData.sections);
   const navdata = await getNavData();
 
   return {
